0a2a7554584948d59847b9dbf32cf8ac11144ad7
abu bakar siddik <mr.siddikdev@gmail.com>
2025-12-01 15:28:19 +0600

    Save

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,4 @@
-import {connectDB} from "@/config/db";
+import connectDB from "@/config/db";
 import { Order } from "@/models/Order";
 import { User } from "@/models/User";
 import { Inngest } from "inngest";
