7c1ebfef2b036a8f74faf7a2f3f81a04626b0a4b
abu bakar siddik <mr.siddikdev@gmail.com>
2025-12-02 23:36:42 +0600

    Save

--- models/Product.js
+++ models/Product.js
@@ -1,19 +1,16 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    offerPrice: { type: Number, required: true },
-    image: { type: [String], required: true }, // array of URLs
-    category: { type: String, required: true },
-    date: { type: Date, default: Date.now }, // store as Date
-  },
-  { timestamps: true }
-);
+const productSchema = new mongoose.Schema({
+  userId: { type: String, required: true, ref: "User" },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  offerPrice: { type: Number, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  date: { type: Number, required: true },
+}, { timestamps: true });
 
-const Product = mongoose.models.product || mongoose.model("Product", productSchema);
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
